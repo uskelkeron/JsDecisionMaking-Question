35ef0cb9a83c66dd14bf902edfb093716cc16616
uskelkeron <k-cyrus@hotmail.com>
2021-11-12 20:36:44 -0400

    Second part of question

--- AgeChecker.js
+++ AgeChecker.js
@@ -1,11 +1,15 @@
 
 const prompt = require("prompt-sync")();
 
-const name = prompt("Please enter your name");
+const name = prompt("Please enter your name: ");
 
-const age = parseInt(prompt("Plese Enter Age"));
+const age = parseInt(prompt("Plese Enter Age: "));
 
-if(age < 18)
+if(age <= 18)
 {
     console.log(`${name}, you are too young to go beyound this point`);
+}
+else
+{
+    console.log(`${name}, you have been granted access`);
 }
