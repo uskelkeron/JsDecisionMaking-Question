790883442aab7dd13a030d2a16865cb81f33aa13
uskelkeron <k-cyrus@hotmail.com>
2021-11-12 20:33:12 -0400

    First Age condition completed

---
+++ AgeChecker.js
@@ -0,0 +1,11 @@
+
+const prompt = require("prompt-sync")();
+
+const name = prompt("Please enter your name");
+
+const age = parseInt(prompt("Plese Enter Age"));
+
+if(age < 18)
+{
+    console.log(`${name}, you are too young to go beyound this point`);
+}
